84bb3640c748ee4fa86dd15fb6c54580db0fdfa9
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-08-03 18:49:03 +0200

    save timestamps

--- www/script.js
+++ www/script.js
@@ -1,6 +1,9 @@
+timetable.innerHTML = localStorage.getItem("timestamps");
+
 function AddWorkTime() {
   let currentDate = myDate.value;
   let startTime = myStarttime.value;
   let endTime = myEndtime.value;
   timetable.innerHTML += `<div class="timestamp">${currentDate} | ${startTime} - ${endTime}</div>`;
+  localStorage.setItem("timestamps", timetable.innerHTML);
 }
