de819dd8330778425d0c543aefbabe3b04c88afc
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-08-03 18:20:48 +0200

    set input values

--- www/script.js
+++ www/script.js
@@ -1,6 +1,6 @@
 function AddWorkTime() {
-  let currentDate = "01-01-2020";
-  let startTime = "8:00";
-  let endTime = "16:00";
-  timetable.innerHTML += `<div class="timestamp">${currentDate} ${startTime} - ${endTime}</div>`;
+  let currentDate = myDate.value;
+  let startTime = myStarttime.value;
+  let endTime = myEndtime.value;
+  timetable.innerHTML += `<div class="timestamp">${currentDate} | ${startTime} - ${endTime}</div>`;
 }
